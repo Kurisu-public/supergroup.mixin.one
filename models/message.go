f78c242b2e97492fba3c400c2d2e32ec427e553e
Amatist_kurisu <misaki.zhcy@gmail.com>
2019-11-22 12:09:40 +0800

    fix:

--- models/message.go
+++ models/message.go
@@ -82,7 +82,7 @@ func CreateMessage(ctx context.Context, user *User, messageId, category, quoteMe
 		return nil, nil
 	}
 	// is white list
-	if !user.isWhiteList() && user.UserId != config.AppConfig.Mixin.ClientId || !user.isAdmin() && user.UserId != config.AppConfig.Mixin.ClientId {
+	if !user.isWhiteList() && !user.isAdmin() && user.UserId != config.AppConfig.Mixin.ClientId {
 		b, err := ReadProhibitedProperty(ctx)
 		if err != nil {
 			return nil, err
